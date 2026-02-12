7c01b686366fe10d6e4134145399727772538544
devu2406 <dr0566@srmist.edu.in>
2026-02-12 10:25:23 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,12 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println(" Welcome to Palindrome Checker App ");
+        System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Version          : 1.0");
+        System.out.println("Description      : This application checks whether");
+        System.out.println("                   a given string is a palindrome.");
+
+        System.out.println("System initialized" + " successfully.");
+    }
+}
+
